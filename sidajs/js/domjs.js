b56ef621284208605ffef020c217b229796fec37
Simlazer <94684874+Simlazer@users.noreply.github.com>
2022-02-09 14:16:13 +0100

    Add files via upload

--- sidajs/js/domjs.js
+++ sidajs/js/domjs.js
@@ -19,3 +19,21 @@ function showDiv(n) {
     }
     imageArray[imageCounter - 1].style.display = "block";
 }
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
